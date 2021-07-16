7cd3ccda5fef3701cea8b3ff19a5b7e5a3a19594
marciojpacheco <87241685+marciojpacheco@users.noreply.github.com>
2021-07-15 22:12:22 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -11,5 +11,8 @@ sudo service docker start
 # Docker Compose Download
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+#Criando imagem Docker
+docker build -f Dockerfile -t uptime/app .
+
 # Executando Docker Compose 
 docker-compose up -d
