5b12dd3a9de51704f7765c90bcc69a656df6a334
marciojpacheco <87241685+marciojpacheco@users.noreply.github.com>
2021-07-15 22:25:53 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,14 @@
 git clone http://github.com/marciojpacheco/desafiosre
 # Docker Download
 sudo yum update
-sudo yum install docker -y
+sudo yum install docker -yy
 sudo service docker start
 
 # Docker Compose Download
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+# Acessando diretório desafiosre
+cd desafiosre
 #Criando imagem Docker
 docker build -f Dockerfile -t uptime/app .
 
