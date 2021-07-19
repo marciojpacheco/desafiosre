548955495e629d7028c6c4c8d2205be6bac1f3b5
marciojpacheco <87241685+marciojpacheco@users.noreply.github.com>
2021-07-19 01:02:51 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+# Instalando git
+
+sudo yum update -yyyy
+sudo yum install git -y
+
 # Download da aplicação, Dockerfile e docker-compose.yaml
-echo "Fazendo download da aplicacao..."
-sudo git clone http://github.com/marciojpacheco/desafiosre /home/ec2-user/desafiosre
+echo -en "Fazendo download da aplicacao..."
+git clone http://github.com/marciojpacheco/desafiosre /home/ec2-user/desafiosre
 
 # Docker Download
-echo "Fazendo download do docker..."
-sudo yum update -y
+echo -en "Fazendo download do docker..."
 sudo yum install docker -yy
 sudo service docker start
-sudo usermod -a -G docker ec2-user
 
 # Docker Compose Download
-echo "Instalando Docker compose..."
+echo -en "Instalando Docker compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo usermod -aG docker ec2-user
+sudo systemctl start docker
 
 # Acessando diretório desafiosre
 cd /home/ec2-user/desafiosre
 #Criando imagem Docker
-echo "Criando imagem docker..."
+echo -en "Criando imagem docker..."
 docker build -f Dockerfile -t uptime/app .
 
 # Executando Docker Compose 
 echo "Executando Docker Compose..."
-docker-compose up -d && sudo docker ps
+docker-compose up -d && docker ps
